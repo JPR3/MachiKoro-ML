7c7879f1e48ef1cd2a8906796e5a74c25560b5ec
Johnny Riganati <jpr3@riganati.com>
2022-01-04 20:49:31 -0500

    Set up basic command structure and 'help' command

---
+++ CommandsHandler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace MachiKoro_ML
+{
+    public class CommandBase
+    {
+        public string commandId;
+        public string commandDescription;
+        public string commandSyntax;
+        public CommandBase(string id, string description, string syntax)
+        {
+            commandId = id;
+            commandDescription = description;
+            commandSyntax = syntax;
+        }
+    }
+    public class Command : CommandBase
+    {
+        private Action command;
+        public Command(string id, string description, string syntax, Action command) : base(id, description, syntax)
+        {
+            this.command = command;
+        }
+        public void Invoke()
+        {
+            command.Invoke();
+        }
+
+    }
+    public class Command<T1> : CommandBase
+    {
+        private Action<T1> command;
+        public Command(string id, string description, string syntax, Action<T1> command) : base(id, description, syntax)
+        {
+            this.command = command;
+        }
+        public void Invoke(T1 value1)
+        {
+            command.Invoke(value1);
+        }
+    }
+    public class Command<T1, T2> : CommandBase
+    {
+        private Action<T1, T2> command;
+        public Command(string id, string description, string syntax, Action<T1, T2> command) : base(id, description, syntax)
+        {
+            this.command = command;
+        }
+        public void Invoke(T1 value1, T2 value2)
+        {
+            command.Invoke(value1, value2);
+        }
+    }
+    public class Command<T1, T2, T3> : CommandBase
+    {
+        private Action<T1, T2, T3> command;
+        public Command(string id, string description, string syntax, Action<T1, T2, T3> command) : base(id, description, syntax)
+        {
+            this.command = command;
+        }
+        public void Invoke(T1 value1, T2 value2, T3 value3)
+        {
+            command.Invoke(value1, value2, value3);
+        }
+    }
+    public class Command<T1, T2, T3, T4> : CommandBase
+    {
+        private Action<T1, T2, T3, T4> command;
+        public Command(string id, string description, string syntax, Action<T1, T2, T3, T4> command) : base(id, description, syntax)
+        {
+            this.command = command;
+        }
+        public void Invoke(T1 value1, T2 value2, T3 value3, T4 value4)
+        {
+            command.Invoke(value1, value2, value3, value4);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MachiKoro_ML
 {
-    class Program
+    class Initialize
     {
-        static void Main(string[] args)
+        public static void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Machi Koro!\r\nType 'help' for a list of commands");
+            Program prog = new Program();
+            prog.ReadCommands();
         }
     }
+    class Program
+    {
+        Command HELP;
+        List<object> commandList;
+
+        public void ReadCommands()
+        {
+            HELP = new Command("help", "shows a list of commands", "help", () =>
+            {
+                for(int i = 0; i < commandList.Count; i++)
+                {
+                    CommandBase commandBase = commandList[i] as CommandBase;
+                    string output = $"{commandBase.commandId} - {commandBase.commandDescription}";
+                    Console.WriteLine(output);
+                }
+            });
+
+            commandList = new List<object>
+            {
+                HELP
+
+            };
+            //Change to only while it should be accepting input
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string[] args = input.Split(' ');
+                for (int i = 0; i < commandList.Count; i++)
+                {
+                    CommandBase commandBase = commandList[i] as CommandBase;
+
+                    if (args[0] == (commandBase.commandId))
+                    {
+                        if (commandList[i] as Command != null && args.Length == 1)
+                        {
+                            Console.WriteLine();
+                            (commandList[i] as Command).Invoke();
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    
 }
